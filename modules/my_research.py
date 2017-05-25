c58e744944708f4d5fe7701a28b9700dec653b22
victoria-yuzkiv <yuzkiv@ucu.edu.ua>
2017-05-25 18:16:43 +0300

    The main research is done.

--- modules/my_research.py
+++ modules/my_research.py
@@ -4,12 +4,13 @@ from modules.series_research import SeriesResearch
 
 class MainResearch:
     """
-
+    This class represents methods which help to do some research.
     """
 
     def __init__(self):
         """
-
+        Reads the information from a file.
+        Makes an array which contains that information.
         """
         file = WorkWithAFile("ratings.txt")
         file1 = file.open_file()
@@ -20,8 +21,9 @@ class MainResearch:
 
     def __str__(self):
         """
+        Creates a string with series titles.
 
-        :return:
+        :return: a string with series titles
         """
         series = ''
         for i in range(self.series.find_len()):
@@ -30,8 +32,13 @@ class MainResearch:
 
     def popular_actors(self):
         """
+        Creates a dictionary. Gets the information about all
+        actors from given series and adds it to the dictionary
+        (key - the name of an actor/actress, value - the number of series
+        in which that actor/actress performed).
 
-        :return:
+        :return: a dictionary (key - the name of an actor/actress, value -
+        the number of series in which that actor/actress performed)
         """
         actors = {}
         for i in range(self.series.find_len()):
@@ -48,8 +55,11 @@ class MainResearch:
 
     def series_seasons(self):
         """
+        Creates a dictionary. Gets the information about number of seasons
+        in series adds it to the dictionary (key - series title, value - the number
+        of seasons).
 
-        :return:
+        :return: a dictionary (key - series title, value - the number of seasons).
         """
         seasons = {}
         for i in range(self.series.find_len()):
@@ -59,26 +69,61 @@ class MainResearch:
                 continue
         return seasons
 
-    def series_names(self):
-        pass
+    def series_titles(self):
+        """
+        Creates a dictionary. Gets every word from series titles and adds it
+        to the dictionary(key - word, value - the number of repetition in
+        series titles).
+
+        :return: the dictionary(key - word, value - the number of
+        repetition in series titles).
+        """
+        words = {}
+        for i in range(self.series.find_len()):
+            word = self.series.get_item(i).split()
+            for k in word:
+                if k not in words:
+                    words[k] = 1
+                else:
+                    words[k] += 1
+        return words
+
+
+def save_info(info, filename):
+    """
+    Creates a new file with a name filename.
+    Writes the information from info into this file.
+
+    :param info: a list of tuples with the information
+    that should be saved.
+    :param filename: a name of a created file.
+    """
+    thefile = open(filename, 'w')
+    for item in info:
+        thefile.write(str(item[1]) + " - " + str(item[0]) + '\n')
 
 
 def main():
     """
-
-    :return:
+    Makes the main research.
     """
-
     series = MainResearch()
-    a = series.popular_actors()
-    for key, val in a.items():
+
+    # actors
+    actors_list = []
+    allactors = series.popular_actors()
+    for key, val in allactors.items():
         if val > 1:
-            print(val, key)
+            actors_list.append((key, val))
+    save_info(sorted(actors_list, key=lambda x: x[1], reverse=True), "popular_actors.txt")
 
-    b = series.series_seasons()
-    print(sorted(b.items(), key=lambda x: x[1]))
+    # seasons
+    allseasons = series.series_seasons()
+    save_info(sorted(allseasons.items(), key=lambda x: x[1], reverse=True), "series_seasons.txt")
 
-    # sorted(numbers.values())
+    # titles
+    alltitles = series.series_titles()
+    save_info(sorted(alltitles.items(), key=lambda x: x[1], reverse=True), "series_titles.txt")
 
 
 main()
