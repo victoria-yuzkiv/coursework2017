6629a58d55cb84a00ee76695bce947513d720733
victoria-yuzkiv <yuzkiv@ucu.edu.ua>
2017-05-26 11:34:27 +0300

    Added setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+# setup.py
+from distutils.core import setup
+setup(name="coursework2017",
+      version="1.0",
+      py_modules=[],
+      packages=['modules', 'examples'],
+      scripts=['modules/my_research.py'],
+      )
